3f11215c436c0e4411a2abb3a8be5964aa6977a9
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-09-16 15:55:39 +1200

    Update version catalog with the dependencies we need

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.detekt) apply false
 }
