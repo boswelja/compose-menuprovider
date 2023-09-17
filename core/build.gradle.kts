7ba69b329c7c955b36dab6b5fb98ad9dcf2463d6
Jack Boswell <boswelja@outlook.com>
2023-09-17 14:45:26 +1200

    Set up publishing (#1)
    * Set up publishing variants
    
    * Add publishing repositories
    
    * Use separate flag for publishing repo config
    
    * Create publish workflow

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     alias(libs.plugins.compose.multiplatform)
 
     alias(libs.plugins.detekt)
+
+    id("maven-publish")
 }
 
 android {
@@ -22,6 +24,13 @@ android {
     lint {
         sarifReport = true
         htmlReport = false
+    }
+
+    publishing {
+        singleVariant("release") {
+            withJavadocJar()
+            withSourcesJar()
+        }
     }
 }
 
@@ -46,4 +55,40 @@ detekt {
     buildUponDefaultConfig = true
     config.setFrom("$rootDir/config/detekt.yml")
     basePath = rootDir.absolutePath
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "io.github.boswelja.menuprovider"
+            artifactId = "core"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+    repositories {
+        if (System.getenv("PUBLISHING") == "true") {
+            maven("https://maven.pkg.github.com/boswelja/Compose-MenuProvider") {
+                val githubUsername: String? by project.properties
+                val githubToken: String? by project.properties
+                name = "github"
+                credentials {
+                    username = githubUsername
+                    password = githubToken
+                }
+            }
+            maven("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
+                val ossrhUsername: String? by project
+                val ossrhPassword: String? by project
+                name = "oss"
+                credentials {
+                    username = ossrhUsername
+                    password = ossrhPassword
+                }
+            }
+        }
+    }
 }
--- material3/build.gradle.kts
+++ material3/build.gradle.kts
@@ -1,9 +1,13 @@
+import org.jetbrains.kotlin.gradle.plugin.extraProperties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
 
     alias(libs.plugins.detekt)
+
+    id("maven-publish")
 }
 
 android {
@@ -22,6 +26,13 @@ android {
     lint {
         sarifReport = true
         htmlReport = false
+    }
+
+    publishing {
+        singleVariant("release") {
+            withJavadocJar()
+            withSourcesJar()
+        }
     }
 }
 
@@ -45,4 +56,40 @@ detekt {
     buildUponDefaultConfig = true
     config.setFrom("$rootDir/config/detekt.yml")
     basePath = rootDir.absolutePath
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "io.github.boswelja.menuprovider"
+            artifactId = "material3"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+    repositories {
+        if (System.getenv("PUBLISHING") == "true") {
+            maven("https://maven.pkg.github.com/boswelja/Compose-MenuProvider") {
+                val githubUsername: String? by project.properties
+                val githubToken: String? by project.properties
+                name = "github"
+                credentials {
+                    username = githubUsername
+                    password = githubToken
+                }
+            }
+            maven("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
+                val ossrhUsername: String? by project
+                val ossrhPassword: String? by project
+                name = "oss"
+                credentials {
+                    username = ossrhUsername
+                    password = ossrhPassword
+                }
+            }
+        }
+    }
 }
