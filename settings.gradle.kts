03d9b37e448244a05e40ae29be73df1ae56d45ff
Jack Boswell <boswelja@outlook.com>
2023-12-26 16:38:14 +1300

    Update AGP (#39)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,8 +16,8 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version("3.16")
-    id("com.android.settings") version("8.1.4")
+    id("com.gradle.enterprise") version("3.16.1")
+    id("com.android.settings") version("8.2.0")
 }
 
 rootProject.name = "Compose MenuProvider"
