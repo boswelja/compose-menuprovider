9dcf1723f3eb25fcbb6adef3c2574cd4f278a87a
Jack Boswell <boswelja@outlook.com>
2024-01-10 18:45:29 +1300

    Create tests for MenuHost implementatons (#46)
    * Fix SingleTopMenuHost not updating values
    
    * Create tests for MenuHost implementations
    
    * Run tests with CI
    
    * Fix pipeline
    
    * Bump version

---
+++ core/src/commonTest/kotlin/com/boswelja/menuprovider/SingleTopMenuHostTest.kt
@@ -0,0 +1,92 @@
+package com.boswelja.menuprovider
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BrokenImage
+import androidx.compose.material.icons.filled.HideImage
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.ImageAspectRatio
+import androidx.compose.material.icons.filled.ImageNotSupported
+import androidx.compose.material.icons.filled.ImageSearch
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class SingleTopMenuHostTest {
+
+    private lateinit var cumulativeMenuHost: SingleTopMenuHost
+
+    @BeforeTest
+    fun setUp() {
+        cumulativeMenuHost = SingleTopMenuHost()
+    }
+
+    @Test
+    fun whenMenuItemsAdded_stateIsUpdated() {
+        val firstBatch = listOf(
+            MenuItem(
+                label = "Test1",
+                imageVector = Icons.Default.Image,
+                onClick = {}
+            ),
+            MenuItem(
+                label = "Test2",
+                imageVector = Icons.Default.ImageSearch,
+                onClick = {}
+            ),
+            MenuItem(
+                label = "Test3",
+                imageVector = Icons.Default.ImageAspectRatio,
+                onClick = {}
+            )
+        )
+        cumulativeMenuHost.addItems(*firstBatch.toTypedArray())
+        assertTrue(cumulativeMenuHost.menuItems.containsAll(firstBatch))
+
+        val secondBatch = listOf(
+            MenuItem(
+                label = "Test4",
+                imageVector = Icons.Default.BrokenImage,
+                onClick = {}
+            ),
+            MenuItem(
+                label = "Test5",
+                imageVector = Icons.Default.ImageNotSupported,
+                onClick = {}
+            ),
+            MenuItem(
+                label = "Test6",
+                imageVector = Icons.Default.HideImage,
+                onClick = {}
+            )
+        )
+        cumulativeMenuHost.addItems(*secondBatch.toTypedArray())
+        assertFalse(cumulativeMenuHost.menuItems.containsAll(firstBatch))
+        assertTrue(cumulativeMenuHost.menuItems.containsAll(secondBatch))
+    }
+
+    @Test
+    fun whenMenuItemsRemoved_stateIsUpdated() {
+        val firstBatch = listOf(
+            MenuItem(
+                label = "Test1",
+                imageVector = Icons.Default.Image,
+                onClick = {}
+            ),
+            MenuItem(
+                label = "Test2",
+                imageVector = Icons.Default.ImageSearch,
+                onClick = {}
+            ),
+            MenuItem(
+                label = "Test3",
+                imageVector = Icons.Default.ImageAspectRatio,
+                onClick = {}
+            )
+        )
+        cumulativeMenuHost.addItems(*firstBatch.toTypedArray())
+        assertTrue(cumulativeMenuHost.menuItems.containsAll(firstBatch))
+        cumulativeMenuHost.removeItems(*firstBatch.toTypedArray())
+        assertTrue(cumulativeMenuHost.menuItems.isEmpty())
+    }
+}
